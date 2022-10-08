1a5e2971d04bdda89009f37d794f07aa43067096
Mirko Mälicke <mirko@hydrocode.de>
2022-10-08 12:43:01 +0200

    adding dark theme

--- src/pages/MainMapTab.tsx
+++ src/pages/MainMapTab.tsx
@@ -2,20 +2,25 @@ import { IonContent, IonHeader, IonPage, IonSpinner, IonTitle, IonToolbar } from
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
 import { useCallback } from 'react';
 
-import ExploreContainer from '../components/ExploreContainer';
-
 const { REACT_APP_GOOGLE_MAPS_KEY } = process.env;
 
 const MainMapTab: React.FC = () => {
+  // get the prefered color settings to load correct mapId
+  const prefer = window.matchMedia('(prefers-color-scheme: dark)')
+
   // load the JS for googlemaps
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: REACT_APP_GOOGLE_MAPS_KEY as string
+    mapIds: ['6060cca46b3b8e75',],
+    googleMapsApiKey: REACT_APP_GOOGLE_MAPS_KEY as string,
   })
 
   // callback run when the maps JS has been loaded and initialized
   const onLoad = useCallback((map: google.maps.Map) => {
     map.setZoom(14)
-    map.setCenter({lat: 48., lng: 7.843})
+    map.setCenter({lat: 48., lng: 7.843});
+    
+    // dev
+    (window as any).map = map
   }, [])
 
   const onUnmount = useCallback(() => {}, [])
@@ -40,6 +45,7 @@ const MainMapTab: React.FC = () => {
             mapContainerStyle={{height: '100%', width: '100%'}}
             onLoad={onLoad}
             onUnmount={onUnmount}
+            options={{ mapId: prefer.matches ? '6060cca46b3b8e75' : '' }}
           ></GoogleMap>
         ) : <IonSpinner name="circular" /> }
 
