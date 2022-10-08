fa0b1cca3fd8c730fa934714ffa2fd3bcaeeff9c
Mirko Mälicke <mirko@hydrocode.de>
2022-10-08 07:18:50 +0200

    map layout

--- src/pages/MainMapTab.tsx
+++ src/pages/MainMapTab.tsx
@@ -1,7 +1,25 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonSpinner, IonTitle, IonToolbar } from '@ionic/react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
+import { useCallback } from 'react';
+
 import ExploreContainer from '../components/ExploreContainer';
 
+const { REACT_APP_GOOGLE_MAPS_KEY } = process.env;
+
 const MainMapTab: React.FC = () => {
+  // load the JS for googlemaps
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: REACT_APP_GOOGLE_MAPS_KEY as string
+  })
+
+  // callback run when the maps JS has been loaded and initialized
+  const onLoad = useCallback((map: google.maps.Map) => {
+    map.setZoom(14)
+    map.setCenter({lat: 48., lng: 7.843})
+  }, [])
+
+  const onUnmount = useCallback(() => {}, [])
+
   return (
     <IonPage>
       <IonHeader>
@@ -10,12 +28,21 @@ const MainMapTab: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Here goes the map" />
+        
+        { isLoaded ? (
+          <GoogleMap
+            mapContainerStyle={{height: '100%', width: '100%'}}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          ></GoogleMap>
+        ) : <IonSpinner name="circular" /> }
+
       </IonContent>
     </IonPage>
   );
