53bfa313580398adb37a44c1afa71ad7943a5b21
Mirko Mälicke <mirko.maelicke@kit.edu>
2022-12-06 14:18:51 +0100

    First working MapBox implementations

--- src/components/MainMapMapbox.tsx
+++ src/components/MainMapMapbox.tsx
@@ -1,9 +1,40 @@
-import { useState } from 'react';
-import Map, { ViewState, ViewStateChangeEvent, Source, Layer } from 'react-map-gl';
+import { useEffect, useState } from 'react';
+import Map, { ViewState, ViewStateChangeEvent, Source, Layer, useMap } from 'react-map-gl';
+import { useData } from '../context/data';
+
+// import mapbox css
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+const InventorySource: React.FC = () => {
+    // get a reference to the map
+    const {current: map} = useMap();
+
+    // subscribe to data context
+    const { inventory } = useData()
+    
+    // flyTo inventory data
+    useEffect(()=> {
+        if (inventory && inventory.bbox) {
+            if (inventory.bbox.length === 4) {
+                map?.fitBounds(inventory.bbox)
+            } else {
+                map?.fitBounds([inventory.bbox[0], inventory.bbox[1], inventory.bbox[3], inventory.bbox[4]])
+            }
+        }
+    }, [inventory, map])
+
+    return <>
+        { inventory ? (
+            <Source id="inventory" type="geojson" data={inventory}>
+                <Layer id="inventory" type="circle" paint={{'circle-color': 'green', 'circle-stroke-color': 'white', 'circle-radius': 8}} />
+            </Source>
+        ) : null }
+        </>
+}
 
 const MainMap: React.FC = () => {
     // get the prefered color settings to load correct mapId
-    const prefer = window.matchMedia('(prefers-color-scheme: dark)')
+    const prefer = window.matchMedia('(prefers-color-scheme: dark)') 
 
     // map state
     const [viewState, setViewState] = useState<Partial<ViewState>>({
@@ -29,9 +60,7 @@ const MainMap: React.FC = () => {
             style={{width: '100%', height: '100%'}}
             mapStyle={prefer ? 'mapbox://styles/mapbox/dark-v10' : 'mapbox://styles/mapbox/light-v10'}
         >
-            <Source id="states" type="raster" tileSize={256} tiles={['http://geowwd.uni-freiburg.de:8080/geoserver/topp/wms?service=WMS&version=1.1.0&request=GetMap&layers=topp%3Astates&bbox=-124.73142200000001%2C24.955967%2C-66.969849%2C49.371735&width=768&height=330&srs=EPSG%3A4326&styles=&format=image%2Fpng']}>
-                <Layer id="states" type="raster" />
-            </Source>
+            <InventorySource />
         </Map>
     )
 }
