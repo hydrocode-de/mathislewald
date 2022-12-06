4c9e43cef7fb43ee03ac7edb1179931f7335a4b2
Mirko Mälicke <mirko.maelicke@kit.edu>
2022-12-06 14:19:01 +0100

    loading inventory correctly

--- src/context/data.tsx
+++ src/context/data.tsx
@@ -9,8 +9,10 @@
  * future changes to the data model
  */
 
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import axios from 'axios';
 import { InventoryData } from "./data.model";
+import { useSettings } from "./settings";
 
 // model the data types
 interface DataState {
@@ -30,12 +32,37 @@ const DataContext = createContext(initialState)
 
 export const DataProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
     // create internal state of the provider
-    const [inventory, setInventory] = useState<InventoryData>();
+    const [inventory, setInventory] = useState<InventoryData>()
 
     // create state for synchronization
-    const [synced, setSynced] = useState<boolean>(false);
+    const [synced, setSynced] = useState<boolean>(false)
 
-    
+    // use the Settings context
+    const { geoserverUrl } = useSettings()
+
+    // create effect to load data
+    useEffect(() => {
+        // run only if settings loaded
+        if (geoserverUrl) {
+            // build query params -> TODO: this should be configurable as well
+            const params = {
+                service: 'WFS',
+                version: '1.0.0',
+                request: 'GetFeature',
+                typeName: 'Public:inventory',
+                outputFormat: 'application/json',
+                srsName: 'EPSG:4326'
+            }
+            axios.get<InventoryData>(`${geoserverUrl}/Public/ows`, {params: params})
+            .then((response => {
+                setInventory(response.data)
+                setSynced(true)
+                console.log(response.data)
+            })).catch(error => console.log(error))
+        } else {
+            console.log('No geoserver url')
+        }
+    }, [geoserverUrl])
 
     // create the final value
     const value = {
