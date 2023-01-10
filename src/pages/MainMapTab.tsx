ef1ed4002331736414fd1c3b6153a7b619d7674a
Mirko Mälicke <mirko@hydrocode.de>
2023-01-10 17:01:50 +0100

    adding first drawer

--- src/pages/MainMapTab.tsx
+++ src/pages/MainMapTab.tsx
@@ -1,23 +1,41 @@
-import { IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonMenu, IonMenuToggle, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { layersOutline, ellipsisHorizontal } from 'ionicons/icons'
 
 //import MainMap from '../components/MainMapGoogle';
 //import MainMap from '../components/MainMapMapbox';
 import MainMap from '../components/MainMapMaplibre';
 
 const MainMapTab: React.FC = () => {
-  return (
-    <IonPage>
+  return <>
+    <IonMenu contentId="main">
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Data Layer</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent class="ion-padding">
+        CONTENT
+      </IonContent>
+    </IonMenu>
+    <IonPage id="main">
       <IonHeader>
         <IonToolbar>
           <IonButtons slot="start">
-
+            <IonMenuToggle>
+              <IonButton>
+                <IonIcon icon={layersOutline} slot="icon-only" />
+              </IonButton>
+            </IonMenuToggle>
           </IonButtons>
           <IonTitle>Mathislewald</IonTitle>
           <IonButtons slot="end">
-
+            <IonButton disabled>
+              <IonIcon icon={ellipsisHorizontal} slot="icon-only" />
+            </IonButton>
           </IonButtons>
         </IonToolbar>
       </IonHeader>
+      
       <IonContent fullscreen>
         
         <IonHeader collapse="condense">
@@ -30,7 +48,7 @@ const MainMapTab: React.FC = () => {
 
       </IonContent>
     </IonPage>
-  );
+  </>
 };
 
 export default MainMapTab;
