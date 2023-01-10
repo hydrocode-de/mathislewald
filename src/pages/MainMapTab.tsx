24a8379e0524086e1c061b1661689611dfffd9cb
Mirko Mälicke <mirko@hydrocode.de>
2023-01-10 16:33:49 +0100

    back to tab layout

--- src/pages/MainMapTab.tsx
+++ src/pages/MainMapTab.tsx
@@ -1,20 +1,28 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import MainMap from '../components/MainMapGoogle';
+import { IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+//import MainMap from '../components/MainMapGoogle';
 //import MainMap from '../components/MainMapMapbox';
+import MainMap from '../components/MainMapMaplibre';
 
 const MainMapTab: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
+          <IonButtons slot="start">
+
+          </IonButtons>
           <IonTitle>Mathislewald</IonTitle>
+          <IonButtons slot="end">
+
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Map</IonTitle>
           </IonToolbar>
         </IonHeader>
         
