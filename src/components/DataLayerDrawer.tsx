80e45480d32ab58c7e7e8a7718fb2f5e14555d6f
Mirko Mälicke <mirko@hydrocode.de>
2023-01-18 20:38:48 +0100

    use DataLayerDrawer

--- src/components/DataLayerDrawer.tsx
+++ src/components/DataLayerDrawer.tsx
@@ -1,33 +1,55 @@
-import { IonItem, IonLabel, IonList, IonListHeader, IonNote, IonSelect, IonSelectOption, IonTitle } from "@ionic/react"
+import { IonCheckbox, IonItem, IonLabel, IonList, IonListHeader, IonTitle } from "@ionic/react"
 import { useLayers } from "../context/layers"
 
 const DataLayerDrawer: React.FC = () => {
     // subscribe to the available Inventories
-    const { availableInventory, availableBaselayer } = useLayers()
+    const layers = useLayers()
 
+    const toggleInventoryLayer = (name: string) => {
+        if (layers.activeInventoryLayer.includes(name)) {
+            layers.deactivateInventoryLayer(name)
+        } else {
+            layers.activateInventoryLayer(name)
+        }
+    }
+
+    const toggleBaseLayer = (name: string) => {
+        if (layers.activeBaseLayer.includes(name)) {
+            layers.deactivateBaseLayer(name)
+        } else {
+            layers.activateBaseLayer(name)
+        }
+    }
+
+    console.log(layers.activeBaseLayer)
+    console.log(layers.activeInventoryLayer)
+    
     return <>
-        <IonList>
             <IonListHeader>
-                <IonTitle>Data</IonTitle>
-            </IonListHeader>
-            
-            <IonItem>
-                <IonLabel>Forest Inventury</IonLabel>
-                <IonSelect>
-                    { availableInventory.map(l => <IonSelectOption key={l.name} value={l.name}>{l.title}</IonSelectOption>) }
-                </IonSelect>
-            </IonItem>
-        </IonList>
-
-        <IonList>
-            <IonListHeader>
-                <IonTitle>Filter</IonTitle>
+                <IonTitle>Forest Inventory</IonTitle>
             </IonListHeader>
 
-            <IonItem>
-                <IonNote>Filters are not yet implemented</IonNote>
-            </IonItem>
-        </IonList>
+            <IonList inset>
+                { layers.availableInventoryLayer.map(l => (
+                    <IonItem key={l.name} onClick={() => toggleInventoryLayer(l.name)}>
+                        <IonLabel>{l.title}</IonLabel>
+                        <IonCheckbox slot="end" checked={layers.activeInventoryLayer.includes(l.name)} />
+                    </IonItem>
+                )) }
+            </IonList>
+
+            <IonListHeader>
+                <IonTitle>Map Layer</IonTitle>
+            </IonListHeader>
+
+            <IonList inset>
+                { layers.availableBaseLayer.map(l => (
+                    <IonItem key={l.name} onClick={() => toggleBaseLayer(l.name)}>
+                        <IonLabel>{l.title}</IonLabel>
+                        <IonCheckbox slot="end" checked={layers.activeBaseLayer.includes(l.name)} />
+                    </IonItem>
+                )) }
+            </IonList>
     </>
 }
 
