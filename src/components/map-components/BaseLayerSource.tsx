689677f1c610e64c173500f713e9c60f74f9c7a6
Mirko Mälicke <mirko@hydrocode.de>
2023-01-24 16:35:10 +0100

    experimental base layers

---
+++ src/components/map-components/BaseLayerSource.tsx
@@ -0,0 +1,22 @@
+import { Layer, Source } from "react-map-gl";
+import { useLayers } from "../../context/layers";
+import * as wms from '../../util/wms';
+
+const BaseLayerSource: React.FC = () => {
+    // subscribe to layers
+    const layers = useLayers()
+
+    console.log(layers.availableBaseLayer)
+    
+    return <>
+        {layers.availableBaseLayer.filter(l => layers.activeBaseLayer.includes(l.name)).map(l => (
+            <Source key={l.name} id={l.name} type={l.name == 'dtm' ? 'raster-dem' : 'raster'} 
+                tiles={[wms.getBaseLayersUri('http://geowwd.uni-freiburg.de/geoserver', l.name)]} tileSize={256}
+            >
+                <Layer id={l.name} type={l.name == 'dtm' ? 'hillshade' : 'raster'} source={l.name}  paint={{}} />
+            </Source>
+        )) } 
+    </>
+}
+
+export default BaseLayerSource;
