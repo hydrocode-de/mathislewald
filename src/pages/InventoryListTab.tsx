8951eb484929ef29b2402d7a647aceabb8f66d29
JesJehle <jesvajnajehle@gmail.com>
2023-01-25 14:55:02 +0100

    fixes

--- src/pages/InventoryListTab.tsx
+++ src/pages/InventoryListTab.tsx
@@ -1,42 +1,59 @@
-import { IonBackButton, IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from "@ionic/react"
-import { useData } from "../context/data"
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { useData } from "../context/data";
 
 const InventoryListTab: React.FC = () => {
-    // subscribe to filtered inventory updates
-    const { filteredInventory } = useData()
+  // subscribe to filtered inventory updates
+  const { filteredInventory } = useData();
 
-    return <>
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonButtons slot="start">
-                        <IonBackButton />
-                    </IonButtons>
-                    <IonTitle>Inventory data list</IonTitle>
-                </IonToolbar>
-            </IonHeader>
+  return (
+    <>
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonBackButton />
+            </IonButtons>
+            <IonTitle>Inventory list</IonTitle>
+          </IonToolbar>
+        </IonHeader>
 
-            <IonContent fullscreen>
+        <IonContent fullscreen>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">Inventory list</IonTitle>
+            </IonToolbar>
+          </IonHeader>
 
-                <IonHeader collapse="condense">
-                    <IonToolbar>
-                        <IonTitle size="large">Incentory data list</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-
-                <IonList>
-                    { filteredInventory ? filteredInventory.features.map(f => {
-                        return (
-                            <IonItem key={f.properties.treeid} routerDirection="forward" routerLink={`/list/${f.properties.treeid}`}>
-                                <IonLabel>Tree ID: {f.properties.treeid}</IonLabel>
-                            </IonItem>
-                        )
-                    }) : null }
-                </IonList>
-
-            </IonContent>
-        </IonPage>
+          <IonList>
+            {filteredInventory
+              ? filteredInventory.features.map((f) => {
+                  return (
+                    <IonItem
+                      key={f.properties.treeid}
+                      routerDirection="forward"
+                      routerLink={`/list/${f.properties.treeid}`}
+                    >
+                      <IonLabel>Tree ID: {f.properties.treeid}</IonLabel>
+                    </IonItem>
+                  );
+                })
+              : null}
+          </IonList>
+        </IonContent>
+      </IonPage>
     </>
-}
+  );
+};
 
-export default InventoryListTab
+export default InventoryListTab;
