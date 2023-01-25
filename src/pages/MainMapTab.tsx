164e2ed19cd0aba9ad207b2777b9d663a411c868
JesJehle <jesvajnajehle@gmail.com>
2023-01-25 13:54:55 +0100

    just some minor fixes in the use of ionic components

--- src/pages/MainMapTab.tsx
+++ src/pages/MainMapTab.tsx
@@ -1,55 +1,68 @@
-import { IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonMenu, IonMenuToggle, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { layersOutline, ellipsisHorizontal } from 'ionicons/icons'
-import DataLayerDrawer from '../components/DataLayerDrawer';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonMenu,
+  IonMenuButton,
+  IonMenuToggle,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { layersOutline, ellipsisHorizontal } from "ionicons/icons";
+import DataLayerDrawer from "../components/DataLayerDrawer";
 
 //import MainMap from '../components/MainMapGoogle';
 //import MainMap from '../components/MainMapMapbox';
-import MainMap from '../components/MainMapMaplibre';
+import MainMap from "../components/MainMapMaplibre";
 
 const MainMapTab: React.FC = () => {
-  return <>
-    <IonMenu contentId="main">
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Data Layer</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent class="ion-padding">
-        <DataLayerDrawer />
-      </IonContent>
-    </IonMenu>
-    <IonPage id="main">
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuToggle>
-              <IonButton>
-                <IonIcon icon={layersOutline} slot="icon-only" />
-              </IonButton>
-            </IonMenuToggle>
-          </IonButtons>
-          <IonTitle>Mathislewald</IonTitle>
-          <IonButtons slot="end">
-            <IonButton disabled>
-              <IonIcon icon={ellipsisHorizontal} slot="icon-only" />
-            </IonButton>
-          </IonButtons>
-        </IonToolbar>
-      </IonHeader>
-      
-      <IonContent fullscreen>
-        
-        <IonHeader collapse="condense">
+  return (
+    <>
+      <IonMenu contentId="main">
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Map</IonTitle>
+            <IonTitle>Data Layer</IonTitle>
           </IonToolbar>
         </IonHeader>
-        
-        <MainMap />
+        <IonContent class="ion-padding">
+          <DataLayerDrawer />
+        </IonContent>
+      </IonMenu>
+      <IonPage id="main">
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonMenuButton></IonMenuButton>
+              {/* <IonButton> */}
+              {/* <IonIcon icon={layersOutline} slot="icon-only" /> */}
+              {/* </IonButton> */}
+              {/* </IonMenuToggle> */}
+            </IonButtons>
+            <IonTitle>Mathislewald</IonTitle>
 
-      </IonContent>
-    </IonPage>
-  </>
+            {/* <IonButtons slot="end">
+              <IonButton disabled>
+                <IonIcon icon={ellipsisHorizontal} slot="icon-only" />
+              </IonButton>
+            </IonButtons>  */}
+          </IonToolbar>
+        </IonHeader>
+
+        <IonContent fullscreen>
+          {/* <IonHeader collapse="condense"> */}
+          {/* <IonToolbar> */}
+          {/* <IonTitle size="large">Map</IonTitle> */}
+          {/* </IonToolbar> */}
+          {/* </IonHeader> */}
+
+          <MainMap />
+        </IonContent>
+      </IonPage>
+    </>
+  );
 };
 
 export default MainMapTab;
