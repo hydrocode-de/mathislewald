d43a586dd798d8bce903c590bed46c5b46d0b3d1
Mirko Mälicke <mirko@hydrocode.de>
2023-01-28 12:05:32 +0100

    DataLayerDrawer fixed

--- src/components/DataLayerDrawer.tsx
+++ src/components/DataLayerDrawer.tsx
@@ -6,7 +6,6 @@ import {
   IonListHeader,
   IonRadio,
   IonRadioGroup,
-  IonTitle,
 } from "@ionic/react";
 import { useLayers } from "../context/layers";
 
@@ -22,66 +21,37 @@ const DataLayerDrawer: React.FC = () => {
     }
   };
 
-  const toggleBaseLayer = (name: string) => {
-    if (layers.activeBaseLayer.includes(name)) {
-      layers.deactivateBaseLayer(name);
-    } else {
-      layers.activateBaseLayer(name);
-    }
-  };
-
-  console.log(layers.activeBaseLayer);
-  console.log(layers.activeInventoryLayer);
-
   return (
     <>
-      {/* <IonListHeader> */}
-      {/* <IonTitle>Forest Inventory</IonTitle> */}
-      {/* </IonListHeader> */}
-
       <IonList inset>
         <IonListHeader>
           <IonLabel>Forest Inventory</IonLabel>
         </IonListHeader>
-        <IonRadioGroup
-          allowEmptySelection={true}
-          value="Inventory:inventory-2022"
-        >
           {layers.availableInventoryLayer.map((l) => (
             <IonItem key={l.name} onClick={() => toggleInventoryLayer(l.name)}>
               <IonLabel>{l.title}</IonLabel>
-              {/* <IonCheckbox slot="end" checked={layers.activeInventoryLayer.includes(l.name)} /> */}
-              <IonRadio
-                slot="end"
-                value={l.name}
-                // ionChange={() => layers.activeInventoryLayer.includes(l.name)}
-              />
+              <IonCheckbox slot="end" checked={layers.activeInventoryLayer.includes(l.name)} />
             </IonItem>
           ))}
-        </IonRadioGroup>
       </IonList>
-
-      {/* <IonListHeader>
-        <IonTitle>Map Layer</IonTitle>
-      </IonListHeader> */}
 
       <IonList inset>
         <IonListHeader>
           <IonLabel>Map Layer</IonLabel>
         </IonListHeader>
-        <IonRadioGroup allowEmptySelection={true}>
+        <IonRadioGroup 
+          allowEmptySelection={true}  
+          onIonChange={e => !!e.detail.value ? layers.setBaseLayerTo([e.detail.value]) : layers.setBaseLayerTo([])}
+        >
           {layers.availableBaseLayer.map((l) => (
-            <IonItem key={l.name} onClick={() => toggleBaseLayer(l.name)}>
+            <IonItem key={l.name}>
               <IonLabel>{l.title}</IonLabel>
-              {/* <IonCheckbox
-              slot="end"
-              checked={layers.activeBaseLayer.includes(l.name)}
-            /> */}
               <IonRadio slot="end" value={l.name}></IonRadio>
             </IonItem>
           ))}
         </IonRadioGroup>
       </IonList>
+
     </>
   );
 };
