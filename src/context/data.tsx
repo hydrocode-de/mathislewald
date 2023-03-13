0a5c0f143cd95ec8db2e9d540320b4a67405ff93
JesJehle <jesvajnajehle@gmail.com>
2023-03-13 12:25:09 +0100

    fixed type error

--- src/context/data.tsx
+++ src/context/data.tsx
@@ -1,11 +1,11 @@
 /**
  * Data context
- * 
+ *
  * This context loads the data into the application.
  * In the final version, it will synchronize the data to localstorage and
  * then load from the localstorage to this context.
- * 
- * The application will only subscribe to this context in order to implement 
+ *
+ * The application will only subscribe to this context in order to implement
  * future changes to the data model
  */
 
@@ -15,76 +15,77 @@ import cloneDeep from "lodash.clonedeep";
 import { useOffline } from "./offline";
 import { InventoryData } from "./data.model";
 
-
 // model the data types
 interface DataState {
-    allInventory: InventoryData | null;
-    filteredInventory: InventoryData | null;
-    synced: boolean;
+  allInventory: InventoryData | null;
+  filteredInventory: InventoryData | null;
+  synced: boolean;
 }
 
 // initial empty state
 const initialState: DataState = {
-    allInventory: null,
-    filteredInventory: null,
-    synced: false
-}
+  allInventory: null,
+  filteredInventory: null,
+  synced: false,
+};
 
 // build the context
-const DataContext = createContext(initialState)
+const DataContext = createContext(initialState);
 
+export const DataProvider: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => {
+  // create internal state of the provider
+  const [allInventory, setAllInventory] = useState<InventoryData>();
+  const [filteredInventory, setFilteredInventory] = useState<InventoryData>();
 
-export const DataProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
-    // create internal state of the provider
-    const [allInventory, setAllInventory] = useState<InventoryData>()
-    const [filteredInventory, setFilteredInventory] = useState<InventoryData>()
+  // create state for synchronization
+  const [synced, setSynced] = useState<boolean>(false);
 
-    // create state for synchronization
-    const [synced, setSynced] = useState<boolean>(false)
+  // use the offline context
+  const { inventory } = useOffline();
 
-    // use the offline context
-    const { inventory } = useOffline()
-
-    // copy over inventory data
-    useEffect(() => {
-        if (inventory) {
-            setAllInventory(inventory)
-        } else {
-            setAllInventory(undefined)
-        }
-    }, [inventory])
-
-    // re-filter inventory when allInventory changes
-    useEffect(() => {
-        if (allInventory) {
-            // TODO build the filter here
-            const inv = {
-                type: 'FeatureCollection',
-                bbox: allInventory?.bbox,  // TODO after filter, update this
-                features: [...cloneDeep(allInventory.features.filter(f => true))]
-            } as InventoryData
-            setFilteredInventory(inv)
-            setSynced(true)
-        } else {
-            setFilteredInventory(undefined)
-        }
-    }, [allInventory])
-
-    // create the final value
-    const value = {
-        allInventory: allInventory || null,
-        filteredInventory: filteredInventory || null,
-        synced
+  // copy over inventory data
+  useEffect(() => {
+    if (inventory) {
+      setAllInventory(inventory);
+    } else {
+      setAllInventory(undefined);
     }
+  }, [inventory]);
 
-    return <>
-        <DataContext.Provider value={value}>
-            { children }
-        </DataContext.Provider>
+  // re-filter inventory when allInventory changes
+  useEffect(() => {
+    if (allInventory) {
+      console.log("allInventory:", allInventory);
+      // TODO build the filter here
+      const inv = {
+        type: "FeatureCollection",
+        bbox: allInventory?.bbox, // TODO after filter, update this
+        features: [...cloneDeep(allInventory.features.filter((f) => true))],
+      } as InventoryData;
+      setFilteredInventory(inv);
+      setSynced(true);
+    } else {
+      setFilteredInventory(undefined);
+    }
+  }, [allInventory]);
+
+  // create the final value
+  const value = {
+    allInventory: allInventory || null,
+    filteredInventory: filteredInventory || null,
+    synced,
+  };
+
+  return (
+    <>
+      <DataContext.Provider value={value}>{children}</DataContext.Provider>
     </>
-}
+  );
+};
 
 // create a hook for easier data access
 export const useData = () => {
-    return useContext(DataContext);
-}
+  return useContext(DataContext);
+};
