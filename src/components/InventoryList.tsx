3987ef6ee33b78f6d05d89be35065773e716ae04
Mirko Mälicke <mirko.maelicke@kit.edu>
2023-03-16 09:50:46 +0100

    add correct distance

--- src/components/InventoryList.tsx
+++ src/components/InventoryList.tsx
@@ -6,39 +6,39 @@ import {
   IonCol,
   IonGrid,
   IonIcon,
-  IonItem,
-  IonItemDivider,
   IonLabel,
-  IonList,
   IonListHeader,
   IonRow,
-  IonSearchbar,
-  IonText,
 } from "@ionic/react";
 import {
   arrowForwardOutline,
-  caretForwardOutline,
-  fileTray,
   filterOutline,
   navigateOutline,
 } from "ionicons/icons";
 import { useState } from "react";
 import { useHistory } from "react-router";
+
+import distance from "@turf/distance"
+
 import { useData } from "../context/data";
-import FilterBar from "./FilterBar";
+import { useSettings } from "../context/settings";
+import { InventoryFeature } from "../context/data.model";
+
 
 const InventoryList: React.FC = () => {
   const [hovered, setHovered] = useState(0);
+
   // load the filtered inventory list
   const { filteredInventory } = useData();
-  console.log(filteredInventory);
 
   // get a history context
   const history = useHistory();
 
+  // get the current position from the Settings
+  const { positionEnabled, position } = useSettings()
+
   const handleOnHover = (id: number) => {
     setHovered(id);
-    console.log(id);
   };
 
   // we need to close the modal on navigate
@@ -46,6 +46,19 @@ const InventoryList: React.FC = () => {
     // navigate forward
     history.push(path);
   };
+
+  const distString = (feature: InventoryFeature) => {
+    if (!position) return ' - '
+    const d = distance(feature.geometry.coordinates, [position.coords.longitude, position.coords.latitude], {units: 'meters'})
+    if (d < 100) {
+      return `${d.toFixed(1)} m`
+    }
+    if (d < 10000) {
+      return `${d.toFixed(0)} m`
+    } else {
+      return `${(d / 1000).toFixed(0)} km`
+    }
+  }
 
   return (
     <>
@@ -59,21 +72,23 @@ const InventoryList: React.FC = () => {
       <IonGrid>
         {filteredInventory?.features.map((f) => {
           return (
-            <IonCard
-            // onClick={() => onNavigate(`/list/${f.properties.treeid}`)}
-            >
+            <IonCard key={f.id}>
               <IonRow class="ion-padding ion-align-items-center">
                 <IonCol>
                   <IonCardSubtitle>TREE ID</IonCardSubtitle>
                   <IonCardTitle>{f.properties.treeid}</IonCardTitle>
                 </IonCol>
-                <IonCol class="ion-text-center">
-                  <IonCardSubtitle>DINSTANCE</IonCardSubtitle>
-                  <IonCardTitle>
-                    <IonIcon icon={navigateOutline} size="small" />
-                    &nbsp;42m
-                  </IonCardTitle>
-                </IonCol>
+                
+                { positionEnabled && position ? (
+                  <IonCol class="ion-text-center">
+                    <IonCardSubtitle>DISTANCE</IonCardSubtitle>
+                    <IonCardTitle>
+                      <IonIcon icon={navigateOutline} size="small" />
+                      &nbsp; {distString(f)}
+                    </IonCardTitle>
+                  </IonCol>
+                ) : null}
+                
                 <IonCol class="ion-text-center ion-hide-sm-down">
                   <IonCardSubtitle>RADIUS</IonCardSubtitle>
                   <IonCardTitle>{f.properties.radius.toFixed(1)}</IonCardTitle>
