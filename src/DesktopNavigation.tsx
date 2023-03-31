56ceed903b3b84b9cc4e98e9e697f1173ee841b2
JesJehle <jesvajnajehle@gmail.com>
2023-03-31 13:57:31 +0200

    adding mobile and Desktop Navigation

--- src/DesktopNavigation.tsx
+++ src/DesktopNavigation.tsx
@@ -21,58 +21,75 @@ import TreeOverviewPage from "./pages/TreeOverviewPage";
 import InventoryListPage from "./pages/InventoryListPage";
 import MapPage from "./pages/MapPage";
 import SettingsPage from "./pages/SettingsPage";
+import DesktopPage from "./pages/DesktopPage";
 
-const MainTabs: React.FC = () => {
-  return (
-    <IonTabs>
-      <IonRouterOutlet>
-        <Redirect exact from="/tabs" to="/tabs/map" />
-        <Route path="/tabs/map" exact>
-          <MapPage />
-        </Route>
-        <Route exact path="/tabs/list">
-          <InventoryListPage />
-        </Route>
-      </IonRouterOutlet>
-      <IonTabBar slot="bottom">
-        <IonTabButton tab="map" href="/tabs/map">
-          <IonLabel>Map</IonLabel>
-          <IonIcon icon={map}></IonIcon>
-        </IonTabButton>
-        <IonTabButton tab="list" href="/tabs/list">
-          <IonLabel>List</IonLabel>
-          <IonIcon icon={listSharp}></IonIcon>
-        </IonTabButton>
-      </IonTabBar>
-    </IonTabs>
-  );
-};
-
-const SplitPaneNavigation: React.FC = () => {
+const MobileNavigation: React.FC = () => {
+  console.log("Running Desktop Navigation");
   return (
     <IonReactRouter>
-      <IonSplitPane when="md" contentId="main">
-        <IonMenu contentId="main">
-          <IonHeader>
-            <IonToolbar>
-              <IonTitle>Data Layer</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <DataLayerDrawer />
-        </IonMenu>
-        <IonRouterOutlet id="main">
-          <Redirect exact from="/" to="/tabs" />
-          <Route path="/tabs">
-            <MainTabs />
-          </Route>
-          <Route path="/list/:id" component={TreeOverviewPage} />
-          <Route exact path="/settings">
-            <SettingsPage />
-          </Route>
-        </IonRouterOutlet>
-      </IonSplitPane>
+      <IonRouterOutlet>
+        <Redirect exact from="/" to="/desktop" />
+        <Route exact path="/desktop">
+          <DesktopPage />
+        </Route>
+      </IonRouterOutlet>
     </IonReactRouter>
   );
 };
 
-export default SplitPaneNavigation;
+export default MobileNavigation;
+
+// const MainTabs: React.FC = () => {
+//   return (
+//     <IonTabs>
+//       <IonRouterOutlet>
+//         <Redirect exact from="/tabs" to="/tabs/map" />
+//         <Route path="/tabs/map" exact>
+//           <MapPage />
+//         </Route>
+//         <Route exact path="/tabs/list">
+//           <InventoryListPage />
+//         </Route>
+//       </IonRouterOutlet>
+//       <IonTabBar slot="bottom">
+//         <IonTabButton tab="map" href="/tabs/map">
+//           <IonLabel>Map</IonLabel>
+//           <IonIcon icon={map}></IonIcon>
+//         </IonTabButton>
+//         <IonTabButton tab="list" href="/tabs/list">
+//           <IonLabel>List</IonLabel>
+//           <IonIcon icon={listSharp}></IonIcon>
+//         </IonTabButton>
+//       </IonTabBar>
+//     </IonTabs>
+//   );
+// };
+
+// const SplitPaneNavigation: React.FC = () => {
+//   return (
+//     <IonReactRouter>
+//       <IonSplitPane when="md" contentId="main">
+//         <IonMenu contentId="main">
+//           <IonHeader>
+//             <IonToolbar>
+//               <IonTitle>Data Layer</IonTitle>
+//             </IonToolbar>
+//           </IonHeader>
+//           <DataLayerDrawer />
+//         </IonMenu>
+//         <IonRouterOutlet id="main">
+//           <Redirect exact from="/" to="/tabs" />
+//           <Route path="/tabs">
+//             <MainTabs />
+//           </Route>
+//           <Route path="/list/:id" component={TreeOverviewPage} />
+//           <Route exact path="/settings">
+//             <SettingsPage />
+//           </Route>
+//         </IonRouterOutlet>
+//       </IonSplitPane>
+//     </IonReactRouter>
+//   );
+// };
+
+// export default SplitPaneNavigation;
