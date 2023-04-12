65e06a39e3ad8614a5031826fb93a0e405354adc
JesJehle <jesvajnajehle@gmail.com>
2023-04-12 15:19:13 +0200

    added activeVariable to inventoryList

--- src/components/InventoryList.tsx
+++ src/components/InventoryList.tsx
@@ -8,7 +8,12 @@ import {
   IonListHeader,
   IonThumbnail,
 } from "@ionic/react";
-import { bookmarkOutline, filterOutline } from "ionicons/icons";
+import {
+  bookmarkOutline,
+  filterOutline,
+  location,
+  navigate,
+} from "ionicons/icons";
 import { useHistory } from "react-router";
 
 import distance from "@turf/distance";
@@ -22,7 +27,7 @@ import { MouseEventHandler } from "react";
 
 const InventoryList: React.FC = () => {
   // load the filtered inventory list
-  const { filteredInventory } = useData();
+  const { filteredInventory, activeVariable } = useData();
 
   // get a history context
   const history = useHistory();
@@ -53,7 +58,9 @@ const InventoryList: React.FC = () => {
     }
   };
 
-  const addToBookmarksHandler = (event: React.MouseEvent<HTMLIonButtonElement, MouseEvent>) => {
+  const addToBookmarksHandler = (
+    event: React.MouseEvent<HTMLIonButtonElement, MouseEvent>
+  ) => {
     event.stopPropagation();
     console.log("add to bookmarks");
   };
@@ -83,14 +90,38 @@ const InventoryList: React.FC = () => {
                 />
               </IonThumbnail>
               <IonLabel>
-                <p> ID: {f.properties.treeid}</p>
-                <div style={{ display: "flex", alignItems: "end" }}>
-                  <h1>{f.properties.height.toFixed(1)}</h1>
-                  <p>{""}m</p>
-                </div>
+                <p style={{ fontSize: 12, paddingBottom: 0, fontWeight: 400 }}>
+                  ID {f.properties.treeid}
+                </p>
+                {activeVariable === "height" ? (
+                  <div style={{ display: "flex", alignItems: "end" }}>
+                    <h1>{f.properties.height.toFixed(1)}</h1>
+                    <p>{""}m</p>
+                  </div>
+                ) : (
+                  <div style={{ display: "flex", alignItems: "end" }}>
+                    <h1>{(f.properties.radius * 100).toFixed(0)}</h1>
+                    <p>{""}cm</p>
+                  </div>
+                )}
               </IonLabel>
               <IonLabel>
-                <p>{distString(f)} away</p>
+                <p
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  <IonIcon
+                    style={{
+                      position: "relative",
+                      bottom: -2,
+                      left: -3,
+                    }}
+                    icon={navigate}
+                  ></IonIcon>
+                  {distString(f)}
+                </p>
               </IonLabel>
               <IonButton fill="clear" onClick={(e) => addToBookmarksHandler(e)}>
                 <IonIcon icon={bookmarkOutline}></IonIcon>
